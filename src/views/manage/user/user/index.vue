c0e2e799605e276d81762f286cdd1f742ffec28d
lijie <crazyjie262@gmail.com>
2020-06-15 11:09:02 +0800

    complex table init

--- src/views/manage/user/user/index.vue
+++ src/views/manage/user/user/index.vue
@@ -1,24 +1,51 @@
 <template>
-  <div class="filter-container">
-    <el-input v-model="listQuery.title" placeholder="用户名" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-    <el-select v-model="listQuery.importance" placeholder="客户状态" clearable style="width: 200px" class="filter-item">
-      <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-    </el-select>
-    <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-      Search
-    </el-button>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-input v-model="listQuery.title" placeholder="用户名" style="width: 200px;" class="filter-item" />
+      <el-select v-model="listQuery.importance" placeholder="客户状态" clearable style="width: 200px" class="filter-item">
+        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search">
+        Search
+      </el-button>
+    </div>
+    <el-table
+      :key="tableKey"
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;margin-top: 20px"
+    >
+      <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80" />
+      <el-table-column label="Date" width="150px" align="center" />
+      <el-table-column label="Title" min-width="150px" />
+      <el-table-column label="Author" width="110px" align="center" />
+      <el-table-column v-if="showReviewer" label="Reviewer" width="110px" align="center" />
+      <el-table-column label="Imp" width="80px" />
+      <el-table-column label="Readings" align="center" width="95" />
+      <el-table-column label="Status" class-name="status-col" width="100" />
+    </el-table>
+    <my />
+    <pagination v-show="total>=0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" />
   </div>
 </template>
 
 <script>
+import waves from '@/directive/waves' // waves directive
+import Pagination from '@/components/Pagination'
+
 export default {
   name: 'User',
+  components: { Pagination },
+  directives: { waves },
   data() {
     return {
       tableKey: 0,
       list: null,
       total: 0,
-      listLoading: true,
+      listLoading: false,
       listQuery: {
         page: 1,
         limit: 20,
