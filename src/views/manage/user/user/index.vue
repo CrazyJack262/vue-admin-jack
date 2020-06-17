692b2ba9cd4da3c367c7ce439d019c5f236380b4
lijie <crazyjie262@gmail.com>
2020-06-17 17:17:13 +0800

    update icon

--- src/views/manage/user/user/index.vue
+++ src/views/manage/user/user/index.vue
@@ -5,20 +5,15 @@
       <el-select v-model="listQuery.userStatus" placeholder="客户状态" clearable style="width: 200px" class="filter-item">
         <el-option v-for="item in userStatusOptions" :key="item.key" :label="item.value" :value="item.key" />
       </el-select>
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleSearch">
         查询
       </el-button>
-      <el-button v-waves class="filter-item" type="primary" icon="icon-add" @click="handleFilter">
+      <el-button v-waves class="filter-item" type="primary" icon="el-third-icon-add-copy" @click="handleAdd">
         新增
-      </el-button>
-      <el-button v-waves class="filter-item" type="primary" icon="icon-delete" @click="handleFilter">
-        删除
-      </el-button>
-      <el-button v-waves class="filter-item" type="primary" icon="icon-update" @click="handleFilter">
-        更新
       </el-button>
     </div>
     <el-table
+      ref="multipleTable"
       :key="tableKey"
       v-loading="listLoading"
       :data="list"
@@ -26,6 +21,7 @@
       fit
       highlight-current-row
       style="width: 100%;margin-top: 20px"
+      @current-change="handleSelectionChange"
     >
       <el-table-column label="ID" prop="id" align="center" min-width="15">
         <template slot-scope="{row}">
@@ -57,13 +53,29 @@
           <span>{{ row.remark }}</span>
         </template>
       </el-table-column>
+      <el-table-column v-if="false" label="版本号">
+        <template slot-scope="{row}">
+          <span>{{ row.version }}</span>
+        </template>
+      </el-table-column>
       <el-table-column label="状态" class-name="status-col" align="center" min-width="30">
         <template slot-scope="{row}">
           {{ statusMap[row.userStatus] }}
         </template>
       </el-table-column>
+      <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="{row,$index}">
+          <el-button type="primary" size="mini" icon="el-third-icon-edit" @click="handleUpdate(row)">
+            编辑
+          </el-button>
+          <el-button v-if="row.status!='deleted'" size="mini" icon="el-third-icon-delete" type="danger" @click="handleDelete(row,$index)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <pagination v-show="total>=0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+
   </div>
 </template>
 
@@ -95,14 +107,32 @@ export default {
         0: '正常',
         1: '异常'
       },
+      textMap: {
+        delete: '删除',
+        update: '更新',
+        create: '新增'
+      },
       dialogPvVisible: false,
-      downloadLoading: false
+      temp: {
+        id: undefined,
+        importance: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        type: '',
+        status: 'published'
+      },
+      downloadLoading: false,
+      multipleTable: [] // 存放选中值的数组
     }
   },
   created() {
     this.getList()
   },
   methods: {
+    handleSelectionChange(val) {
+      this.multipleTable = val
+    },
     getList() {
       this.listLoading = true
       fetchList(this.listQuery).then(response => {
@@ -115,10 +145,37 @@ export default {
         }, 1.5 * 260)
       })
     },
-    handleFilter() {
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        importance: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        status: 'published',
+        type: ''
+      }
+    },
+    handleSearch() {
       this.listQuery.page = 1
       this.getList()
+    },
+    handleAdd() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    handleDelete() {
+      alert('delete')
+    },
+    handleUpdate(row) {
+      console.log(row)
+      alert('update')
     }
+
   }
 }
 </script>
